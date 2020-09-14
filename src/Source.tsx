2ad48e8e65e573cc119d696f6c467f21f126b844
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-09-14 19:34:08 +0200

    Add component to display spec in json and yaml

--- src/Source.tsx
+++ src/Source.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import spec from "./spec.json";
 import YAML from "yaml";
+import Editor from "@monaco-editor/react";
+
+enum Notation {
+  JSON = "JSON",
+  YAML = "YAML",
+}
+
 const Source = () => {
-  console.log(spec);
+  const [notation, setNotation] = useState(Notation.JSON);
+
   return (
     <div>
-      <pre>{JSON.stringify(spec)}</pre>
-      {YAML.stringify(spec)}
+      <div>
+        <h2>Notation:</h2>
+        <button onClick={() => setNotation(Notation.JSON)}>JSON</button>
+        <button onClick={() => setNotation(Notation.YAML)}>YAML</button>
+      </div>
+      <Editor
+        height="90vh"
+        language={notation === Notation.JSON ? "json" : "yaml"}
+        value={
+          notation === Notation.JSON
+            ? JSON.stringify(spec, null, 2)
+            : YAML.stringify(spec)
+        }
+      />
     </div>
   );
 };
